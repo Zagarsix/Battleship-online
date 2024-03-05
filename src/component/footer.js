bef0bccbef0d9bca950ef95c1cbbe3a654ad3171
Zagarsix <claudk1006@gmail.com>
2024-03-05 03:24:13 -0300

    adding footer component

---
+++ src/component/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logoCC from "../img/iconDark.jpg"
+
+const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center">
+        <img className="mx-3" src={logoCC} alt="Zagarsix" width="60" height="50"></img>
+		<span>&copy; 2024 - Developed with <i className="fa fa-heart text-danger" /> by {""}
+			<a href="https://github.com/Zagarsix">Zagarsix</a>
+		</span>
+	</footer>
+);
+
+export default Footer;
